fccd8f7c537777e9e2a6b2903a6c39b3ac40696c
Valentin Dauvier <valentindauvier@hotmail.fr>
2021-05-02 18:11:07 +0200

    agence web

---
+++ main.js
@@ -0,0 +1,104 @@
+const lancementDate = new Date('may 25, 2021 18:32:00').getTime();
+
+function newAgence() {
+
+  const now = new Date().getTime();
+
+  gap = lancementDate - now;
+
+  const secondes = 1000;
+  const minutes = secondes * 60;
+  const heures = minutes * 60;
+  const jours = heures * 24;
+
+  const d = Math.floor(gap / (jours));
+  const h = Math.floor((gap % (jours)) / (heures));
+  const m = Math.floor((gap % (heures)) / (minutes));
+  const s = Math.floor((gap % (minutes)) / (secondes));
+
+  document.getElementById('jours').innerText = d;
+  document.getElementById('heures').innerText = h;
+  document.getElementById('minutes').innerText = m;
+  document.getElementById('secondes').innerText = s;
+
+}
+
+setInterval(function () {
+  newAgence();
+
+}, 1000)
+
+
+
+
+
+
+
+
+const allRonds = document.querySelectorAll('.rond');
+const allBoxes = document.querySelectorAll('.box');
+
+const controller = new ScrollMagic.Controller()
+
+allBoxes.forEach(box => {
+
+  for (i = 0; i < allRonds.length; i++) {
+
+    if (allRonds[i].getAttribute('data-anim') === box.getAttribute('data-anim')) {
+
+      let tween = gsap.from(box, { y: -50, opacity: 0, duration: 0.5 })
+
+      let scene = new ScrollMagic.Scene({
+        triggerElement: allRonds[i],
+        reverse: true
+      })
+        .setTween(tween)
+        .addIndicators()
+        .addTo(controller)
+
+    }
+
+  }
+
+})
+
+
+
+
+
+
+
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+
+}
+
+
+var captionText = document.getElementById("caption");
+captionText.innerHTML = dots[slideIndex - 1].alt;
